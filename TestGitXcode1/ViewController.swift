4bbc464f4df990c00efe4346afdae8eedce9b8fb
Montacer <montacer.el-fazazi.external@capgemini.com>
2024-04-09 10:31:20 +0200

    Initial Commit

---
+++ TestGitXcode1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGitXcode1
+//
+//  Created by Montacer El Fazazi on 9/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
